efdf82299baab5bb6d3c31aac27666d0aab412a1
LC-GHub <92838415+LC-GHub@users.noreply.github.com>
2022-01-11 11:37:47 +0800

    OOP_lab1

---
+++ Lab01_example04.java
@@ -0,0 +1,18 @@
+
+public class Lab01_example04 {
+
+	public static void main(String[] args) {
+		
+		for(int x = 102; x>= 66 ; x=x-1)
+		{
+			if(x%2 != 0)
+			{
+				System.out.print("Value of X: " + x);
+				System.out.println();
+			}
+			
+		}
+
+	}
+
+}
---
+++ Lab01_example1.java
@@ -0,0 +1,9 @@
+
+public class Lab01_example1 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello, I am Lionel");
+
+	}
+
+}
